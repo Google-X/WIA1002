4a6265381579e8c48893d3cf734aa72899f3f742
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-02-19 10:52:26 +0800

    T1Q4 SmartPhone @ 190220, 10:52

---
+++ src/T1/SmartPhone.java
@@ -0,0 +1,27 @@
+/*
+ * 4. Create a class SmartPhone that implements the comparable interface. The class consists of model name and the price of the model. Create an array for multiple SmartPhone objects. Sort the smartphone in ascending order of the price. Example output:
+
+List of Smart Phone:
+iPhone (RM 2400) Samsung (RM 2200) Nokia (RM 1400) LG (RM 1800)
+List of Smart Phone in ascending order of the price
+Nokia (RM1400) LG (RM 1800) Samsung (RM 2200) iPhone (RM 2400)
+ */
+package T1;
+
+public class SmartPhone implements SmartPhoneComparable{
+    private String modelName;
+    private int price;
+
+    public SmartPhone(String modelName, int price) {
+        this.modelName = modelName;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+    
+    public String toString() {
+        return modelName + " (RM " + price + ") ";
+    }
+}
