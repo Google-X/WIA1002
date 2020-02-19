500a6ce36c032d1ce5b6a4a094e1aa3876eaf477
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-02-19 09:08:26 +0800

    T1Q1 Tarikh @ 190220, 09:08

--- src/T1/Tarikh.java
+++ src/T1/Tarikh.java
@@ -1,10 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 1. Create a class called Tarikh that includes three instance variables type integer. (day, month, year). Create a constructor that initializes the three instance variables. Create a toString() method that return the date format in "dd/mm/yyyy". Example output:
+
+Day: 5 Month: 11 Year: 2016
+Date is : 05/11/2016
  */
 package T1;
 
 public class Tarikh {
+    
+    private int day, month, year;
+
+    public Tarikh(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public String toString() {
+        return "Date is : " + day + "/" + month + "/" + year;
+    }
+
+    
     
 }
