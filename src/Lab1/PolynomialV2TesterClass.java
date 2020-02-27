5e5f68e24a0559226cc92f5b52b234e353815788
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-02-27 16:36:45 +0800

    L1Q2 Polynomial V2 Tester Class @ 270220 16:36

---
+++ src/Lab1/PolynomialV2TesterClass.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab1;
+
+/**
+ *
+ * @author User
+ */
+public class PolynomialV2TesterClass {
+    public static void main(String[] args) {
+
+        double[] a = {4.0, 2.0, -0.5, -20.0};
+        PolynomialV2 p1 = new PolynomialV2(3, a);
+        System.out.print("The polynomial is ");
+        p1.display();
+        System.out.println();
+        p1.calculate(2);
+        System.out.println();
+        p1.calculate(-3.5);
+        System.out.println();
+
+    }
+}
