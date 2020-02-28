aaa138377a19ed11dac197c7d5439319395c9871
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-02-28 17:41:25 +0800

    L1Q3 Simple Network Question update @ 280220 17:40

--- src/Lab1/SimpleNetworkTesterClass.java
+++ src/Lab1/SimpleNetworkTesterClass.java
@@ -4,7 +4,7 @@
 Host Name: Host 1 IP: 10.1.1.1 Subnet Mask: 255.255.255.224 Status: UP
 Host Name: Host 2 IP: 10.1.1.2 Subnet Mask: 255.255.255.224 Status: DOWN
 Host Name: Host 3 IP: 10.1.1.70 Subnet Mask: 255.255.255.224 Status: UP
-Host Name: Host 4 IP: 10.1.1.1 Subnet Mask: 255.255.255.224 Status: UP
+Host Name: Host 4 IP: 10.1.1.15 Subnet Mask: 255.255.255.224 Status: UP
 Host 1 cannot ping Host 2 because the destination Host 2 is down.
 Host 1 cannot ping Host 3 because the destination Host 3 is located in different network.
 Host 1 can ping Host 4.
@@ -18,7 +18,7 @@ public class SimpleNetworkTesterClass {
         s[0] = new SimpleNetwork("Host 1", "10.1.1.1", "255.255.255.224", true);
         s[1] = new SimpleNetwork("Host 2", "10.1.1.2", "255.255.255.224", false);
         s[2] = new SimpleNetwork("Host 3", "10.1.1.70", "255.255.255.224", true);
-        s[3] = new SimpleNetwork("Host 4", "10.1.1.1", "255.255.255.224", true);
+        s[3] = new SimpleNetwork("Host 4", "10.1.1.15", "255.255.255.224", true);
         
         for(int i = 0; i < s.length; i++){
             System.out.println(s[i].toString());
