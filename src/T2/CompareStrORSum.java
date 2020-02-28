f39cf5cb4c3f63f6c7c6d9d4ec61d28288098be9
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-02-28 19:23:49 +0800

    T2Q1 Compare the String or addition of the objects [Need Correction] @ 280220 19:23

--- src/T2/CompareStrORSum.java
+++ src/T2/CompareStrORSum.java
@@ -7,21 +7,73 @@ The parameters value are : 12.4 and 64.3
 The sum of the double is 76.7
 The parameters value are : 20 and 35
 The sum of  the integer is 55
-The parameters calue are : Welcome and Welcome
+The parameters value are : Welcome and Welcome
 The string is equal
 
  */
 package T2;
 
+class Tester{
+    public static void main(String[] args) {
+        
+        CompareStrORSum<String, String> a = new CompareStrORSum<>("Hello", "World");
+        CompareStrORSum<Double, Double> b = new CompareStrORSum<>(12.4, 64.3);
+        
+        // Object as the type is possible
+        CompareStrORSum<Object, Object> c = new CompareStrORSum<>(20, 35);
+        CompareStrORSum<Object, Object> d = new CompareStrORSum<>("Welcome", "Welcome");
+    }
+}
+
 public class CompareStrORSum<T, U> {
     private T t;
     private U u;
-
+    
     public CompareStrORSum(T t, U u) {
         this.t = t;
         this.u = u;
+        
+        if(checkDataType()){
+            switch (t.getClass().getSimpleName()) {
+                case "String":
+                    System.out.println(toString());
+                    checkSpelling();
+                    break;
+                case "Integer":
+                    System.out.println(toString());
+                    addInt();
+                    break;
+                case "Double":
+                    System.out.println(toString());
+                    addDouble();
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            System.out.println("Different type of data.");
+        }
     }
-
+    
+    public boolean checkDataType(){
+        return t.getClass().getSimpleName().equals(u.getClass().getSimpleName());
+    }
+    
+    public void checkSpelling(){
+        if(t.equals(u)) System.out.println("The string is equal.");
+        else System.out.println("The string is not equal.");
+    }
+    
+    public void addInt(){
+        System.out.print("The sum of the " + t.getClass().getSimpleName().toLowerCase() + " is ");
+        System.out.println( (T)(Integer)((Integer) t + (Integer) u));
+    }
+    
+    public void addDouble(){
+        System.out.print("The sum of the " + t.getClass().getSimpleName().toLowerCase() + " is ");
+        System.out.println( (T)(Double)((Double) t + (Double) u));
+    }
+    
     public void setT(T t) {
         this.t = t;
     }
@@ -37,11 +89,8 @@ public class CompareStrORSum<T, U> {
     public U getU() {
         return u;
     }
-    
-}
 
-class Tester{
-    public static void main(String[] args) {
-        CompareStrORSum<String, String> a = new CompareStrORSum<>("Hello", "World");
-    }
+    public String toString() {
+        return "The parameter value are : " + t + " and " + u;
+    }   
 }
