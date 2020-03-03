6303bb0a554718daf50a8828cb5c82155c26dee7
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-03-03 17:41:18 +0800

    T2Q4 Array Dice @ 030320 17:41

---
+++ src/T2/ArrayDice.java
@@ -0,0 +1,76 @@
+/*
+ * 4. Create an ADT Dice which contains the specifications below: 
+ A method that rolls the dice for a number of times. 
+ A toString method that returns the value of each dice rolled and the total value.  
+Implement the ADT Dice using ArrayDice and LinkedListDice. Example output: 
+
+Array Implementation: Roll 5 times(s)
+5 1 3 2 5
+Total is 16
+Array Implementation: Roll 4 times(s)
+2 2 3 2
+Total is 9
+Linked List Implementation: Roll 3 time(s)
+5 1 1
+Total is 7
+Linked List Implementation: Roll 4 time(s)
+2 5 2 1
+Total is 10
+
+ */
+package T2;
+
+import java.util.Random;
+
+// Without "extends Comparable<T>" also can
+// With "extends Comparable" also can
+public class ArrayDice<T extends Comparable<T>> implements ADTDice<T>{
+    
+    private T[] arr;
+    private int index, maxSize;
+    private Random r = new Random();
+    
+    public ArrayDice(int numOfRoll) {
+        index = -1;
+        maxSize = numOfRoll;
+        arr = (T[]) new Comparable[numOfRoll]; // What is the meaning of number in the square here?
+    }
+    
+    public void rollDice() {
+        for(int i = 0; i < maxSize; i++){
+            insert((T)(Integer)(1 + r.nextInt(6)));
+        }
+    }
+    
+    public void insert(T t) {
+        if(!isFull()){
+            index++;
+            arr[index] = t;
+//            arr[index] = r.nextInt(5); // <-- why cannot?
+        } else {
+            System.out.println("The log is full");
+        }
+    }
+    
+    public boolean isFull() {
+        return maxSize == (index + 1);
+    }
+    
+    public int size() {
+        return index + 1;
+    }
+    
+    public String toString() {
+        int total = 0;
+        String temp = "Array Implementation: Roll " + maxSize + " time(s)\n";
+        
+        for(int i = 0; i < arr.length; i++){
+            temp += arr[i] + " ";
+            total += (Integer) arr[i];
+        }
+        
+        temp += "\nTotal is " + total;
+        
+        return temp;
+    }
+}
