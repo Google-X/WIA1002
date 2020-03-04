265d19afca1c114b974bf6fcfc6c310a483b0f1e
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-03-05 00:05:09 +0800

    L2Q1 Non Duplicate Generator Tester Class Question included @ 050320 00:04

--- src/Lab2/NonDuplicateGeneratorTesterClass.java
+++ src/Lab2/NonDuplicateGeneratorTesterClass.java
@@ -1,7 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 1. Create a class that used to generate N number of non-duplicate integer in between 0 – 100. The class must display the number in sorted list. The class must be generic to support array and linked-list implementation. Example output:  
+Generate 20 non-duplicate integer within 0-100
+Linked List Implementation
+0 10 14 18 27 31 35 43 49 50 52 54 55 61 67 76 81 84 97 100
+Array Implementation
+1 5 6 15 17 20 33 39 44 47 49 52 53 69 744 78 82 85 96 98
  */
 package Lab2;
 
