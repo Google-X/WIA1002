b35f62f0ac36d1590314c87380e716c7cd9bc0e7
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-03-07 17:21:53 +0800

    T3 [Basic Recursion] Tower of Hanoi removed "Done" @ 070320 17:20

--- src/T3/TowerOfHanoi.java
+++ src/T3/TowerOfHanoi.java
@@ -16,7 +16,7 @@ public class TowerOfHanoi {
             System.out.printf("%sMove ring %d from %s to %s\n", indent, n, start, end);
             completeWalkthrough(n-1, temp, start, end);
             indent = indent.substring(2);
-            System.out.print("Done\n\n");
+//            System.out.print("Done\n\n");
         }
     }
     
