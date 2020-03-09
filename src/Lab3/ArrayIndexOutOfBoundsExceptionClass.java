18ad61b03f66348a25c91c696d17fbc699b58314
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-03-09 08:46:39 +0800

    L3Q5 [Exception] Problem in the catching InputMismatchException @ 090320 08:46

---
+++ src/Lab3/ArrayIndexOutOfBoundsExceptionClass.java
@@ -0,0 +1,54 @@
+/*
+ * 5. 5. Write a program that creates an integer array with 5 elements. The program accepts user input for the elements of the array. Use exception handling to detect improper inputs. The user needs to enter the correct input to continue. The program will stop if ArrayIndexOutOfBoundsException is encountered. The program will display all integers entered by the user. Example output:
+
+Enter an Integer: 12
+Enter an Integer: hello
+Invalid input type
+Enter an Integer: 23
+Enter an Integer: 86.7
+Invalid input type
+Enter an Integer: 32
+Enter an Integer: 10
+Enter an Integer: 92
+Enter an Integer: 2
+The array of integers is:
+12 23 32 10 92
+ */
+package Lab3;
+
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class ArrayIndexOutOfBoundsExceptionClass {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        int[] arr = new int[5];
+        int i = 0;
+        
+        while(true){
+            
+            try{
+                System.out.print("Enter an Integer: ");
+                arr[i] = s.nextInt();
+                s.nextLine();
+                i++;
+            
+            // Problem now is that if you enter the wrong data type again, the program stops.
+            } catch (InputMismatchException ime){
+                System.out.println("Invalid input type");
+                System.out.print("Enter an Integer: ");
+                s.nextLine();
+                arr[i] = s.nextInt();
+                i++;
+            } catch (ArrayIndexOutOfBoundsException ofb){
+                System.out.println(Arrays.toString(arr).replace("[^0-9\\s+]", ""));
+                break;
+            }
+            
+        }
+        
+    }
+}
