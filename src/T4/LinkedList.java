50e8edbaa297969b69b76643744ec8f767ea8fce
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-03-18 21:20:59 +0800

    L4Q5 & Q6 [LinkedList] Random number generator and finding the median of the list, bubble sort needs to be added @ 180320 21:21

--- src/T4/LinkedList.java
+++ src/T4/LinkedList.java
@@ -1,6 +1,8 @@
 
 package T4;
 
+import java.util.Random;
+
 public class LinkedList<T extends Comparable<T>> {
     
     private ListNode head;
@@ -30,8 +32,8 @@ public class LinkedList<T extends Comparable<T>> {
         int counter = 0;
         ListNode currentNode = head;
         while(currentNode != null){
-            counter++;
             currentNode = currentNode.getLink();
+            counter++;
         }
         return counter;
     }
@@ -67,14 +69,41 @@ public class LinkedList<T extends Comparable<T>> {
     }
     
     public void addFrontNode(T data){
-        head = new ListNode(data, head);
+        head = new ListNode(data, head); // the link cannot be null, as the link need to be link to the previous head
     }
     
-    public void removeFrontNode(){
+    public void deleteFrontNode(){
         if(head != null){
             head = head.getLink();
         } else {
             System.out.println("The list is empty");
+        }
+    }
+    
+    public void addNodeByPosition(T data, int index){
+        if(index == 0) addFrontNode(data);
+        else if (index == length()-1) addNode(data);
+        else if(index >= length()) System.out.println("Invalid index. No node is added");
+        else {
+            ListNode newNode = new ListNode(data, null);
+            ListNode currentListNode = head;
+            for(int i = 1; i < index; i++) currentListNode = currentListNode.getLink();
+            ListNode tempNode = currentListNode.getLink();
+            currentListNode.setLink(newNode);
+            newNode.setLink(tempNode);
+        }
+    }
+    
+    public void deleteNodeByPosition(int index){
+        if(index == 0) deleteFrontNode();
+        else if (index == length()-1) deleteNode();
+        else if(index >= length()) System.out.println("Invalid index. No node is deleted.");
+        else {
+            ListNode currentListNode = head;
+            for(int i = 1; i < index; i++) currentListNode = currentListNode.getLink();
+            ListNode tempNode = currentListNode.getLink();
+            currentListNode.setLink(tempNode.getLink());
+            tempNode = null;
         }
     }
     
@@ -164,4 +193,65 @@ public class LinkedList<T extends Comparable<T>> {
         }
     }
     
+// Q4 methods
+    public T sum(){
+        int ttl = 0;
+        ListNode currentNode = head;
+        if(head != null){
+            while(currentNode != null){
+                ttl += (Integer)currentNode.getData();
+                currentNode = currentNode.getLink();
+            }
+        }
+        return (T)(Integer)ttl;
+    }
+    
+    public T even(){
+        int counter = 0;
+        ListNode currentNode = head;
+        if(head != null){
+            while(currentNode != null){
+                if((Integer)currentNode.getData() % 2 == 0) counter++;
+                currentNode = currentNode.getLink();
+            }
+        }
+        return (T)(Integer)counter;
+    }
+    
+// Q5 methods
+    public void generateList(int n, int start, int end){
+        Random r = new Random();
+        for(int i = 0; i < n; i++){
+            addNode((T)(Integer)(r.nextInt(end - start + 1) + start));
+        }
+        System.out.printf("Linked List has %d node(s)\n", length());
+        showList();
+    }
+    
+    public void deleteAll(T data){
+        System.out.println("Delete " + data + " from the list.");
+        ListNode currentNode = head;
+        if(head == null){
+            System.out.println("There is nothing in the list");
+        } else {
+            for(int i = 0; i < length(); i++){
+                if(data.compareTo((T)currentNode.getData()) == 0)
+                    deleteNodeByPosition(i);
+                currentNode = currentNode.getLink();
+            }
+        }
+        System.out.printf("Linked List has %d node(s)\n", length());
+        showList();
+    }
+    
+// Q6 method
+    public void showMedian(){
+        // NOTE: get(int index)
+        
+        if(length() % 2 == 0){
+            System.out.println("The medians are: " + get(length()/2 - 1) + " & " + get((length()/2)));
+        } else {
+            System.out.println("The median is " + get(length()/2));
+        }
+    }
 }
