53cedbcfb46a073e49e3351a6c57745553059b59
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-03-20 11:47:13 +0800

    [LinkedList] Test time Shorter algorithm but slower execution of removeLast() @ 200320 11:34

--- src/T4/TestTime.java
+++ src/T4/TestTime.java
@@ -6,12 +6,30 @@ package T4;
 public class TestTime {
 
     public static void main(String[] args) {
-
+// SHORTER SHORTER SHORTER SHORTER SHORTER SHORTER SHORTER SHORTER SHORTER SHORTER SHORTER
+        long deleteNodeShort = 0l;
+        LinkedList<Integer> b = new LinkedList<>();
+        for(int i = 0; i < 50; i++){
+            b.addNode(i);
+        }
+        System.out.println("\nShorter Algorithm");
+        b.showList();
+        for(int i = 0; i < 50; i++){
+            long t = System.nanoTime();
+            b.deleteNodeShorter();
+            t = System.nanoTime() - t;
+            deleteNodeShort += t;
+        }
+        System.out.println("50 Loop time taken(ns): " + deleteNodeShort);
+        System.out.println("Average time taken(ns): " + (Double)(deleteNodeShort/50.0));
+        
+//// LONGER LONGER LONGER LONGER LONGER LONGER LONGER LONGER LONGER LONGER LONGER LONGER LONGER
         long deleteNode = 0l;
         LinkedList<Integer> a = new LinkedList<>();
         for(int i = 0; i < 50; i++){
             a.addNode(i);
         }
+        System.out.println("\nLonger Algorithm");
         a.showList();
         for(int i = 0; i < 50; i++){
             long t = System.nanoTime();
@@ -21,24 +39,5 @@ public class TestTime {
         }
         System.out.println("50 Loop time taken(ns): " + deleteNode);
         System.out.println("Average time taken(ns): " + (Double)(deleteNode/50.0));
-        
-        
-// SHORTER SHORTER SHORTER SHORTER SHORTER SHORTER SHORTER SHORTER SHORTER SHORTER SHORTER
-        long deleteNodeShort = 0l;
-        LinkedList<Integer> b = new LinkedList<>();
-        for(int i = 0; i < 50; i++){
-            b.addNode(i);
-        }
-        System.out.println("");
-        b.showList();
-        for(int i = 0; i < 50; i++){
-            long t = System.nanoTime();
-            b.deleteNode();
-            t = System.nanoTime() - t;
-            deleteNodeShort += t;
-        }
-        System.out.println("50 Loop time taken(ns): " + deleteNodeShort);
-        System.out.println("Average time taken(ns): " + (Double)(deleteNodeShort/50.0));
-
     }
 }
