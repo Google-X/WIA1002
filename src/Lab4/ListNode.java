58dadb910711e9e9e38f7d1a98dac3848022f08f
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-03-22 13:31:07 +0800

    L4Q1 [LinkedList] Normal ListNode class @ 220320 13:30

---
+++ src/Lab4/ListNode.java
@@ -0,0 +1,40 @@
+/*
+ * ListNode for all classes in Lab 4 questions.
+ */
+package Lab4;
+
+public class ListNode<E> {
+    private E data;
+    private ListNode link;
+
+    public ListNode(E data, ListNode link) {
+        this.data = data;
+        this.link = link;
+    }
+    
+    public ListNode() {
+        data = null;
+        link = null;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public ListNode getLink() {
+        return link;
+    }
+
+    public void setLink(ListNode link) {
+        this.link = link;
+    }
+
+    @Override
+    public String toString() {
+        return data + " --> ";
+    }
+}
