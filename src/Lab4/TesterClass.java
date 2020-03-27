dd35fd4264a1cf2fd63c65d75ad9ad9793e8d34e
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-03-27 09:59:51 +0800

    L4Q1 [LinkedList] Fun with numbers @ 270320 09:59

--- src/Lab4/TesterClass.java
+++ src/Lab4/TesterClass.java
@@ -1,126 +1,5 @@
 /*
- * Lab 4 Questions are at below
- */
-package Lab4;
-
-import java.util.Random;
-import java.util.Scanner;
-
-public class TesterClass {
-    public static void main(String[] args) {
-        /*
-//Question 1
-        System.out.println("Question 1");
-        Random r = new Random();
-        /**
-         * b = addLastNode()
-         * f = addFirstNode();
-         * s = sort the list
-         */
-        /*
-        LinkedList<Byte> b = new LinkedList();
-        LinkedList<Byte> f = new LinkedList();
-        LinkedList<Byte> s = new LinkedList();
-        
-        System.out.print("The random numbers are : ");
-        for(byte i = 0; i < 10; i++){
-            byte num = (byte) r.nextInt(101);
-            System.out.print(num + " ");
-            b.addLastNode(num);
-            f.addFirstNode(num);
-            s.addSortNode(num);
-        }
-        
-        System.out.println("\nInsert the random numbers at the back of the linked list");
-        b.showList();
-        
-        System.out.println("\nInsert the random numbers at the front of the linked list");
-        f.showList();
-        
-        System.out.println("\nInsert the random numbers in a sorted linked list");
-        s.showList();
-        System.out.println();
-        
-// Question 2
-        Scanner sc = new Scanner(System.in);
-        System.out.println("\nQuestion 2");
-        System.out.print("Enter a word : ");
-        String q2temp = sc.nextLine();
-        LinkedList<Character> q2 = new LinkedList<>();
-        for(char c : q2temp.toCharArray()) q2.addLastNode(c);
-        
-        System.out.println("The original list : ");
-        q2.showList();
-        
-        // 2a. splitList()
-        LinkedList<Character>[] split = q2.splitList();
-        System.out.println("\n\nSplit the list into two");
-        LinkedList<Character> splitList1 = split[0];
-        LinkedList<Character> splitList2 = split[1];
-        System.out.print("First List : ");
-        splitList1.showList();
-        System.out.print("\nSecond List : ");
-        splitList2.showList();
-        
-        // 2b. alternateList()
-        LinkedList<Character>[] alternate = q2.alternateList();
-        System.out.println("\n\nSplit the list by alternating the nodes");
-        LinkedList<Character> alternateList1 = alternate[0];
-        LinkedList<Character> alternateList2 = alternate[1];
-        System.out.print("First List : ");
-        alternateList1.showList();
-        System.out.print("\nSecond List : ");
-        alternateList2.showList();
-        
-        // 2c. mergeList()
-        System.out.println("\n\nMerge First List and Second List by alternating the nodes");
-        alternateList1.mergeList(alternateList2);
-        alternateList1.showList();
-        
-        // 2d. reverse();
-        System.out.println("\n\nReverse the list. Recursive method in the LinkedList");
-        alternateList1.reverse(alternateList1.length());
-        alternateList1.showList();
-        
-        // 2e. reverse recursive in class
-        System.out.println("\n\nReverse the list. Recursive method in tester class");
-        reverseBack(alternateList1, alternateList1.length());
-        alternateList1.showList();
-        
-// Question 3
-        Course[] c = new Course[3];
-        c[0] = new Course("WXX101", "Programming", 5, 'B');
-        c[1] = new Course("WXX201", "Networking", 4, 'C');
-        c[2] = new Course("WXX301", "Operating Systems", 3, 'A');
-        CourseLinkedList<Course> student1 = new CourseLinkedList<>();
-        for(Course i : c) student1.addLastNode(i);
-        student1.showList();
-        c[2].showSummary();
-        */
-    }
-    
-    public static void reverseBack(LinkedList list, int size){
-        if(list.length()%2 == 0){
-            if(size >= list.length()/2 + 1){
-                Comparable tmp = list.get(size-1);
-                list.set(list.get(list.length()-size), size-1);
-                list.set(tmp, list.length()-size);
-                reverseBack(list, size-1);
-            }
-        } else {
-            if(size > list.length()/2){
-                Comparable tmp = list.get(size-1);
-                list.set(list.get(list.length()-size), size-1);
-                list.set(tmp, list.length()-size);
-                reverseBack(list, size-1);
-            }
-        }
-    }
-}
-
-
-/*
-1. Create the ListNode.java and LinkedList.java file (only the methods required based on the lecture notes). Then create a tester class that generate 10 random numbers within 0 – 100. After that, insert the numbers into the linked list as below: 
+ * 1. Create the ListNode.java and LinkedList.java file (only the methods required based on the lecture notes). Then create a tester class that generate 10 random numbers within 0 – 100. After that, insert the numbers into the linked list as below: 
 a. Insert the random number at the back of the linked list. 
 b. Insert the random number in front of the linked list.  
 c. Insert the random number in a sorted linked list. You need to create a new method addSortNode in the LinkedList.java.  
@@ -136,50 +15,38 @@ Insert the random numbers at the front of the linked list
 
 Insert the random numbers in a sorted linked list
 4 --> 7 --> 14 --> 17 --> 28 --> 34 --> 40 --> 61 --> 82 --> 84
+ */
+package Lab4;
 
+import java.util.Random;
 
-2. Write a program that inserts the characters of a sentence into a linked list. Then, perform the following: 
-a. Create a new method splitList in the LinkedList.java. If the number of characters is odd, the extra character is inserted in the first list. 
-b. Create a new method alternateList in the LinkedList.java. Split the list into two by alternating the elements from the original list. 
-c. Create a new method mergeList in the LinkedList.java. Merge two lists into one by alternating the elements from both of the lists. 
-d. Create a recursive method in the LinkedList.java to reverse the list.   
-e. Create a static recursive method in the tester class to reverse the list.  
-Example output
-        
-Enter a word : Banking
-The original list : B --> a --> n --> k --> i --> n --> g -->
+public class TesterClass {
+    public static void main(String[] args) {
+        Random r = new Random();
+        /**
+         * b = addLastNode(); f = addFirstNode(); s = sort the list
+         */
+        LinkedList<Byte> b = new LinkedList();
+        LinkedList<Byte> f = new LinkedList();
+        LinkedList<Byte> s = new LinkedList();
 
-Split the list into two
-First List : B --> a --> n --> k -->
-Second List : i --> n --> g -->
+        System.out.print("The random numbers are : ");
+        for (byte i = 0; i < 10; i++) {
+            byte num = (byte) r.nextInt(101);
+            System.out.print(num + " ");
+            b.addLastNode(num);
+            f.addFirstNode(num);
+            s.addSortNode(num);
+        }
 
-Split the list by alternating the nodes
-First List : B --> n --> i --> g -->
-Second List : a --> k --> n -->
+        System.out.println("\nInsert the random numbers at the back of the linked list");
+        b.showList();
 
-Merge First List and Second List by alternating the nodes
-B --> a --> n --> k --> i --> n --> g -->
+        System.out.println("\nInsert the random numbers at the front of the linked list");
+        f.showList();
 
-Reverse the list. Recursive method in the LinkedList
-g --> n --> i --> k --> n --> a --> B -->
-
-Reverse the list. Recursive method in tester class
-B --> a --> n --> k --> i --> n --> g -->
-
-
-3. Create a class Course that consists of the course code, course name, credit hours and the grade of the course (A, B, C, D and F). The point allocate to each grade is (4, 3, 2, 1 and 0). Then, insert the following information into a linked list. After that, calculate the grade point average for the student. 
-
-Example output
-Course Code         Course Name         Credit Hours        Grade 
-WXX101              Programming         5                   B 
-WXX201              Networking          4                   C
-WXX301              Operating System    3                   A
-
-The list consist of
-Course : WXX101 (Programming) - 5 credit hours. Grade : B -->
-Course : WXX201 (Networking) - 4 credit hours. Grade : C -->
-Course : WXX301 (Operating System) - 3 credit hours. Grade : A -->
-Total point is 35
-Total credit is 12
-Grade point average is 2.92
-*/
+        System.out.println("\nInsert the random numbers in a sorted linked list");
+        s.showList();
+        System.out.println();
+    }
+}
