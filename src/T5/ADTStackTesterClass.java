3bb53494aac91e3684e63505e8d37f718633ba56
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-03-29 20:39:42 +0800

    T5Q2 [Stack] Stack implementation using Array @ 290320 20:39

---
+++ src/T5/ADTStackTesterClass.java
@@ -0,0 +1,39 @@
+/*
+ * 2. Create an ADT stack named ArrayStack. The ADT consists of the following method and the maximum size of the ADT is 10. a. Constructor b. isEmpty c. isFull d. getSize e. Push f. Pop g. Peek h. showStack    
+Example output: 
+
+Stack implementation using Array
+Insert three nodes into the stack
+ <-- D <-- N <-- A
+Pop one node from the stack
+ <-- N <-- A
+The element on the top of the stack is N
+ <-- N <-- A
+The number of elements in the stack is 2
+
+ */
+package T5;
+
+public class ADTStackTesterClass {
+    public static void main(String[] args) {
+        
+        System.out.println("Stack implementation using Array");
+        char[] chList = "AND".toCharArray();
+        
+        Stack<Character> st = new Stack<>();
+        for(char c : chList) st.push(c);
+        
+        System.out.println("Insert three nodes into the stack");
+        st.showStack();
+        
+        System.out.println("\nPop one node from the stack");
+        st.pop();
+        st.showStack();
+        
+        System.out.println("\nThe element on the top of the stakc is " + st.peek());
+        st.showStack();
+        
+        System.out.println("\nThe number of elements in the stack is " + st.getSize());
+        
+    }
+}
