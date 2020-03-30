5cc3bbce95a7f43b459e09c1e0ef2ee52074de85
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-03-30 08:23:02 +0800

    L5Q1 [Stack] Stack Class @ 300320 08:22

---
+++ src/Lab5/Stack.java
@@ -0,0 +1,55 @@
+/*
+ * Lab 5: Stack Class
+ */
+package Lab5;
+
+public class Stack<E> {
+    
+    private ListNode head;
+
+    public Stack() {
+        head = null;
+    }
+    
+    public boolean isEmpty(){
+        return head == null;
+    }
+    
+    public int getSize(){
+        int count = 0;
+        ListNode c = head;
+        while(c != null) {
+            count++;
+            c = c.getLink();
+        }
+        return count;
+    }
+    
+    public void push(E e){
+        head = new ListNode(e, head);
+    }
+    
+    public E pop(){
+        if(head == null) return null;
+        E tmp = (E) head.getData();
+        head = head.getLink();
+        return tmp;
+    }
+    
+    public E peek(){
+        if(head == null) return null;
+        return (E) head.getData();
+    }
+    
+    public void showStack(){
+        if(isEmpty()) System.out.println("The stack is empty");
+        else {
+            ListNode c = head;
+            while(c != null){
+                System.out.print(c.toString());
+                c = c.getLink();
+            }
+        }
+    }
+    
+}
