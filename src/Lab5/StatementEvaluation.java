889ca5d131432790ae2f0e5d81d4522f3f5b72f2
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-04-12 12:49:00 +0800

    L5Q4 [Stack] Evaluates parentheses @ 120420 12:50

--- src/Lab5/StatementEvaluation.java
+++ src/Lab5/StatementEvaluation.java
@@ -23,43 +23,73 @@ package Lab5;
 import java.util.Scanner;
 
 public class StatementEvaluation {
+    
+    static char[] open = {'(', '{', '['};
+    static char[] close = {')', '}', ']'};
+    
     public static void main(String[] args) {
         
-        Stack<Character> st = new Stack<>(true);
+        Stack<Character> st = new Stack<>();
         Scanner s = new Scanner(System.in);
         
         System.out.print("Enter an expression : ");
         String str = s.nextLine();
-        int space = 0;
-        boolean missing = false;
         boolean extra = false;
+        char[] li = str.toCharArray();
+        int i = 0;
         
-        for(char c : str.toCharArray()) {
-            space++;
-            if(c == ';') {
-                if(st.pushParen(c) > 0) {
-                    missing = true;
+        for(i = 0; i < li.length; i++){
+            if(li[i] == '\\') i++;
+            else if(li[i] == ';') break;
+            else {
+                if(checkOpenParenthesis(li[i])) st.push(li[i]);
+                else if(checkCloseParenthesis(li[i])){
+                    if(st.getSize() > 0 && open[getIndex(li[i])] == st.peek()) st.pop();
+                    else if(st.getSize() == 0) {
+                        st.push(li[i]);
+                        extra = true;
+                        break;
+                    }
+                    else break;
                 }
-            } else {
-                if (st.pushParen(c) < 0) {
-                    extra = true;
-                    break;
-                } 
             }
         }
         
-        if(missing){
-            System.out.println(str);
-            for(int i=0;i<space-1;i++) System.out.print(" ");
-            System.out.println("^ Missing " + st.check());
-        } else if (extra){
-            System.out.println(str);
-            for(int i=0;i<space;i++) System.out.print(" ");
-            System.out.println("^ Extra " + st.check());
-        } else {
-            System.out.println(str);
+        // Final Output
+        System.out.println(str);
+        if(st.getSize() == 0 && !extra){
             System.out.println("The expression is balanced");
+        } else {
+            for(int j = 0; j < i; j++) System.out.print(" ");
+            if(extra){
+                System.out.println("^ Extra " + close[getIndex(st.peek())]);
+            } else {
+                System.out.println("^ Missing " + close[getIndex(st.peek())]);
+            }
         }
         
+        
+        
+    }
+    
+    public static boolean checkOpenParenthesis(char c){
+        for(int i = 0; i < open.length; i++){
+            if(open[i] == c) return true;
+        }
+        return false;
+    }
+    
+    public static boolean checkCloseParenthesis(char c){
+        for(int i = 0; i < close.length; i++){
+            if(close[i] == c) return true;
+        }
+        return false;
+    }
+    
+    public static int getIndex(char c){
+        for(int i = 0; i < close.length; i++){
+            if(close[i] == c || open[i] == c) return i;
+        }
+        return -1;
     }
 }
