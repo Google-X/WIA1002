dd0490d55fa31b78e2f59e1950b7d6f99d3d521a
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-04-14 23:18:22 +0800

    T6Q5 [Queue] Fun with REGEX huh? @ 140420 23:18

--- src/T6/Palindrome.java
+++ src/T6/Palindrome.java
@@ -8,6 +8,15 @@ Was it a car or a cat I saw? is not a palindrome
 civic is a palindrome
 race car is not a palindrome
 
+5. Modify Question 4 to ignore the capital letter, punctuation and space characters.   
+Example output:   
+
+Was it a car or a cat I saw? is a palindrome
+?civic# is a palindrome
+race car is a palindrome
+redrawer is not a palindrome
+put      it up is a palindrome
+
  */
 package T6;
 
@@ -16,21 +25,38 @@ import T5.Stack;
 public class Palindrome {
     public static void main(String[] args) {
         
+        // Question 4
+        System.out.println("Question 4");
         String[] list = {"kayak", "Welcome", "Was it a car or a cat I saw?", "civic", "race car"};
+        check(list, list);
         
-        for(String s : list){
+        
+        // Question 5
+        System.out.println("\n\nQuestion 5");
+        String[] list2 = {"Was it a car or a cat I saw?", "?civic#", "race car", "redrawer", "put      it up"};
+        String[] list2Tmp = new String[list2.length];
+        for(int i = 0; i < list2.length; i++){
+            list2Tmp[i] = list2[i].toLowerCase();
+            list2Tmp[i] = list2Tmp[i].replaceAll("[^a-zA-Z0-9]", "");
+        }
+        check(list2, list2Tmp);
+        
+    }
+    
+    public static void check(String[] list, String[] tmp){
+        for(int i = 0; i < list.length; i++){
             Queue<Character> Q = new Queue<>();
             Stack<Character> S = new Stack<>();
-            for(char c : s.toCharArray()) {
+            for(char c : tmp[i].toCharArray()) {
                 Q.enqueue(c);
                 S.push(c);
             }
             String Qu = Q.showQueuePalindrome();
             String St = S.showStackPalindrome();
             
-            if(Qu.equals(St)) System.out.println(s + " is a palindrome");
-            else System.out.println(s + " is not a palindrome");
+            if(Qu.equals(St)) System.out.println(list[i] + " is a palindrome");
+            else System.out.println(list[i] + " is not a palindrome");
             
-        }   
+        }
     }
 }
