013c99a538a795fa8edefc7c6efa2c7181fb7919
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-04-15 09:47:56 +0800

    L6Q1 [Queue] Queue Class @ 150420 09:47

---
+++ src/Lab6/Queue.java
@@ -0,0 +1,60 @@
+/*
+ * Queue Class
+ */
+package Lab6;
+
+public class Queue<T> {
+    
+    private ListNode head;
+
+    public Queue() {
+        head = null;
+    }
+
+    public boolean isEmpty(){
+        return head == null;
+    }
+    
+    public int getSize(){
+        int i = 0;
+        ListNode c = head;
+        while(c != null){
+            i++;
+            c = c.getLink();
+        }
+        return i;
+    }
+    
+    public void enqueue(T t){
+        ListNode n = new ListNode(t, null);
+        if(head == null) head = n;
+        else {
+            ListNode c = head;
+            while(c.getLink() != null) c = c.getLink();
+            c.setLink(n);
+        }
+    }
+    
+    public T dequeue(){
+        if(head == null) return null;
+        T t = (T) head.getData();
+        head = head.getLink();
+        return t;
+    }
+    
+    public T peek(){
+        if(head == null) return null;
+        return (T) head.getData();
+    }
+    
+    public void showQueue(){
+        if(head == null) System.err.println("EmptyQueueException");
+        else {
+            ListNode c = head;
+            while(c != null){
+                System.out.print(c.toString());
+                c = c.getLink();
+            }
+        }
+    }
+}
