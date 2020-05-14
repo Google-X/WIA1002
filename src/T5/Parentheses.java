bcdb7ced34b1750a72b98d8c156cdbb6bbe1dd83
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-05-14 16:57:27 +0800

    T5Q5 [Stack] Easier way to determine a balance parentheses by using two stacks @ 140520 16:55

--- src/T5/Parentheses.java
+++ src/T5/Parentheses.java
@@ -12,22 +12,56 @@ Example output:
 package T5;
 
 public class Parentheses {
+
     public static void main(String[] args) {
-        
+
         String[] li = {
             "(())",
             ")()(",
             "((())())()",
             "(())()))",
-            "())",
-        };
-        
-        for(int i = 0; i < li.length; i++){
-            char[] cl = li[i].toCharArray();
-            Stack<Character> typing = new Stack<>();
-            for(char c : cl) typing.push(c);
-            System.out.println(typing.balanceParentheses() ? li[i] + " is balanced" : li[i] + " is not balanced");
+            "())",};
+
+        for (int i = 0; i < li.length; i++) {
+            balance(li[i]);
         }
         
+        /*
+        for (int i = 0; i < li.length; i++) {
+            char[] cl = li[i].toCharArray();
+            Stack<Character> typing = new Stack<>();
+            for (char c : cl) {
+                typing.push(c);
+            }
+            System.out.println(typing.balanceParentheses() ? li[i] + " is balanced" : li[i] + " is not balanced");
+        }
+        */
+    }
+
+    public static void balance(String s) {
+        Stack<String> list = new Stack<>();
+        Stack<String> list2 = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                list.push(s);
+            }
+
+            if (s.charAt(0) == ')') {
+                list.pop();
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                list2.push(s);
+            }
+        }
+
+        if (list.getSize() == list2.getSize()) {
+            System.out.println(s + " is balanced");
+        } else {
+            System.out.println(s + " is not balanced");
+        }
     }
 }
