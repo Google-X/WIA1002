b294edd9c655ee5ada6164bb322d39cc013ce7ff
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-05-21 22:26:19 +0800

    L7Q1 [BST] Normal BSTNode Class @ 210520 22:25

---
+++ src/Lab7/BSTNode.java
@@ -0,0 +1,45 @@
+
+package Lab7;
+
+public class BSTNode<T extends Comparable<T>> {
+    
+    private T data;
+    private BSTNode<T> leftLink;
+    private BSTNode<T> rightLink;
+
+    public BSTNode() {
+        data = null;
+        leftLink = null;
+        rightLink = null;
+    }
+
+    public BSTNode(T t, BSTNode<T> l, BSTNode<T> r) {
+        data = t;
+        leftLink = l;
+        rightLink = r;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T t) {
+        data = t;
+    }
+
+    public BSTNode<T> getLeft() {
+        return leftLink;
+    }
+
+    public void setLeft(BSTNode<T> l) {
+        leftLink = l;
+    }
+
+    public BSTNode<T> getRight() {
+        return rightLink;
+    }
+
+    public void setRight(BSTNode<T> r) {
+        rightLink = r;
+    }
+}
