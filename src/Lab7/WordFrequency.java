4860335f388da92722714c220338fa72329e97e5
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-05-22 10:17:40 +0800

    L7Q2 [BST] Minor trimming @ 220520 10:17

--- src/Lab7/WordFrequency.java
+++ src/Lab7/WordFrequency.java
@@ -11,8 +11,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class WordFrequency {
 
