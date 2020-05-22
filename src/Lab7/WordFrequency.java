3d4707a4c95c02592a3084adec343741681e5645
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-05-22 10:12:25 +0800

    L7Q2 [BST] To find the frequency of a word in a sentence @ 220520 10:11

---
+++ src/Lab7/WordFrequency.java
@@ -0,0 +1,40 @@
+/*
+ * 2. Create a word frequency generator program. The program will read a text file (lab7Q2.txt) and generate an alphabetical listing of the unique words that the file contains, along with a count of how many times each word occurs.   
+Example output: 
+
+
+
+ */
+package Lab7;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordFrequency {
+
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("C:/Users/User/Documents/NetBeansProjects/WIA1002/src/Lab7/Source/lab7Q2.txt");
+        String str = "";
+
+        try (BufferedReader s = new BufferedReader(new FileReader(file))) {
+            for (String line = s.readLine(); line != null; line = s.readLine()) {
+                // use trim() to remove whitespaces 
+                str += line + "\n";
+            }
+        }
+        
+        str = str.replaceAll("[^a-zA-Z0-9\\s+]", "");
+        System.out.println(str);
+
+        String[] word = str.split(" ");
+        BST<String> bst = new BST<>();
+        for(String w : word) bst.addNode(w);
+        bst.wordCounter();
+        
+    }
+}
