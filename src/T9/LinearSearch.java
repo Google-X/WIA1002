7025f38076be8d7788111ce65642a0c27de12338
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-06-10 22:31:49 +0800

    T9Q1 [Search] Linear Search of an element and in between two elements @ 100620 22:31

---
+++ src/T9/LinearSearch.java
@@ -0,0 +1,55 @@
+/*
+ * 1. Write a program that generates 10 random characters. Then, perform the following using Linear Search: 
+a. A method that returns true if the element can be found. 
+b. A method that returns the number of occurrence for the element. 
+c. A method that returns a list of index for the element. 
+d. A method that returns true if the elements can be found. (search by range) 
+e. A method that returns the number of occurrence for the elements. (search by range) 
+f. A method that returns a list of index for the elements. (search by range)  
+Example output: [BELOW]
+
+ */
+package T9;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        SearchTest<Character> searcher = new SearchTest<>('A', 10, 0);
+        
+        System.out.print("The Character data set are : ");
+        searcher.showValue();
+        
+        System.out.println("\nLinear Search");
+        System.out.print("Enter a letter to search: ");
+        char c = s.next().charAt(0);
+        
+        if(searcher.linearSearch(c)){
+            System.out.printf("%c is found\n", c);
+            int count = searcher.linearSearchCount(c);
+            System.out.printf("The number of %c in the data set is %d\n", c, count);
+            System.out.printf("The location(s) of the %c is/are : ", c);
+            for(int i : searcher.getLinearSearchIndex(c)) System.out.print(i + " ");
+            System.out.println();
+        } else {
+            System.out.printf("%c is not found\n", c);
+        }
+        
+        System.out.print("\nEnter two letters to search (begin end) : ");
+        char c1 = s.next().charAt(0);
+        char c2 = s.next().charAt(0);
+        
+        if(searcher.linearSearchBetween(c1, c2)){
+            System.out.printf("The character can be found in between %c and %c\n", c1, c2);
+            int count = searcher.linearSearchCountBetween(c1, c2);
+            System.out.printf("The number of the elements in between %c and %c in the data set is %d\n", c1, c2, count);
+            System.out.printf("The location(s) of the elements in between %c and %c in the data set is/are : ", c1, c2);
+            for(int i : searcher.getLinearSearchIndexBetween(c1, c2)) System.out.print(i + " ");
+            System.out.println();
+        } else {
+            System.out.printf("No character can be found in between %c and %c\n", c1, c2);
+        }
+    }
+}
